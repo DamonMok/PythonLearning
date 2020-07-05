f7cbf62ac410be06ac334896091e9a10c260531d
DamonMok <damonmok@Damon.local>
2020-07-05 17:53:04 +0800

    web服务器_http协议_浏览器和服务器请求和响应的数据格式

---
+++ web服务器_http协议_浏览器和服务器请求和响应的数据格式.py
@@ -0,0 +1,46 @@
+"""
+
+浏览器---》服务器
+GET / HTTP/1.1  # 这句必须要有
+Host: www.baidu.com
+Connection: keep-alive
+Cache-Control: max-age=0
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+Cookie: PSTM=1593166817; BAIDUID=A2BE9729C5E20D1A290D8F788FF59160:FG=1; BD_UPN=123253; BIDUPSID=A078B771438634CD3C15509A566D8159;
+BDUSS=jViTEZNamV4Zk80WE05UGxRZlpHM3Z3M3h1WkxHOEZId21INU45Y20zLVJ1QjVmRVFBQUFBJCQAAAAAAAAAAAEAAAAY8FhLbW9ldmlzdQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJEr916RK~deb;
+BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; BDRCVFR[feWj1Vr5u3D]=I67x6TjHwwYf0; delPer=0; BD_CK_SAM=1; PSINO=1; BD_HOME=1; cflag=13%3A3; H_PS_PSSID=32096_1436_31326_32139_31253_32046_32230_31639;
+shifen[150747731791_6685]=1593940797; BCLID=11946328892471955395; BDSFRCVID=m4KOJeC62mrp6mRr0OKv2mkxheajrUjTH6aI-fAF1hX_zBCqvblBEG0P_x8g0KubZaqWogKK3gOTH4AF_2uxOjjg8UtVJeC6EG0Ptf8g0f5;
+H_BDCLCKID_SF=tJCfoItKtC83MDP9Mn__-P4Depu8aMRZ5mAqoJ6y5Cbaj4_xe-RJD4uf2M7-JfRtQ2OnaIQqa56BElOEDUtB0joyjxKJBMn43bRTKnLy5KJvfJ_RjR8ahP-UyNbMWh37Jg6lMKoaMp78jR093JO4y4Ldj4oxJpOJ5JbMopCafD_BMK_
+Gej0BenI3qxoj--PXKKOLVbC2Wh7keq8CD4cKK5KD0lKjWMnqtDjWXnnHKRcKSJc2y5jHyDuELtDDapvdMermQ4QtapopsIJMBnAWbT8U5f5z5RoUaKviaKOjBMb1MhjDBT5h2M4qMxtOLR3pWDTm_q5TtUJMeCnTD-Dhe4tX-NFJJ50fJU5;
+COOKIE_SESSION=2254_3_8_2_12_9_0_0_8_3_1_0_20732_5639_3395_3391_1593940802_1593940798_1593937407%7C9%232254_28_1593937407%7C7
+
+
+服务器---》浏览器
+HTTP/1.1 200 OK  # 这句必须要有
+Bdpagetype: 2
+Bdqid: 0xa6464bb50010e1c6
+Cache-Control: private
+Connection: keep-alive
+Content-Encoding: gzip
+Content-Type: text/html;charset=utf-8
+Date: Sun, 05 Jul 2020 09:46:42 GMT
+Expires: Sun, 05 Jul 2020 09:46:41 GMT
+Server: BWS/1.1
+Set-Cookie: BDSVRTM=250; path=/
+Set-Cookie: BD_HOME=1; path=/
+Set-Cookie: H_PS_PSSID=32096_1436_31326_32139_31253_32046_32230_31639; path=/; domain=.baidu.com
+Strict-Transport-Security: max-age=172800
+Traceid: 1593942402020747981811981347099428053446
+X-Ua-Compatible: IE=Edge,chrome=1
+Transfer-Encoding: chunked
+
+
+"""
