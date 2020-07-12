1b5ed6174ba8b429a7ac26e66b57a43a736b81a0
DamonMok <damonmok@Damon.local>
2020-07-12 22:06:20 +0800

    Mini-Web框架_WSGI-mini-web框架_3.实现简单的框架，让web服务器支持

---
+++ mini_frame.py
@@ -0,0 +1,17 @@
+import time
+
+
+def login():
+    return "login--%s" % time.ctime()
+
+
+def register():
+    return "register--%s" % time.ctime()
+
+
+def other():
+    return "other--%s" % time.ctime()
+
+
+def application():
+    return "Hello,Python1!%s" % time.ctime()
