f9bac14da72898022b950c78d00a939808b9a7ae
DamonMok <damonmok@Damon.local>
2020-07-12 19:19:29 +0800

    MySQL数据库_MySQL与Python交互_3.防止SQL注入

--- MySQL数据库_MySQL与Python交互_2.增加、删除、修改数据.py
+++ MySQL数据库_MySQL与Python交互_2.增加、删除、修改数据.py
@@ -15,28 +15,31 @@ def main():
     # count = cursor.execute('update student set name="张大猪" where id=3')
 
     # 3.3 删除数据并返回受影响的函数
-    # count = cursor.execute('delete from student where id=4')
-    # print(count)
+    count = cursor.execute('delete from student where id=4')
+    print(count)
 
-    # 4 执行查询，并返回受影响的函数
+    # 4 增删改后，commit之后才会真正的起作用
+    conn.commit()
+
+    # 5 执行查询，并返回受影响的函数
     count = cursor.execute('select * from student')
     print("查询到%d条数据" % count)
 
-    # 5.获取查询到的数据
-    # 5.1 cursor.fetchone() 一条一条地取结果
+    # 6.获取查询到的数据
+    # 6.1 cursor.fetchone() 一条一条地取结果
     # for i in range(count):
     #     rst = cursor.fetchone()
     #     print(rst)
 
-    # 5.2 fetchmany()  # 默认一条条取，有指定参数就按照参数的数量取
+    # 6.2 fetchmany()  # 默认一条条取，有指定参数就按照参数的数量取
     # rst = cursor.fetchmany(3)
     # print(rst)
 
-    # 5.3 fetchall()  # 全部取出
+    # 6.3 fetchall()  # 全部取出
     rst = cursor.fetchall()
     print(rst)
 
-    # 6.关闭游标对象、关闭数据库连接
+    # 7.关闭游标对象、关闭数据库连接
     cursor.close()
     conn.close()
 
