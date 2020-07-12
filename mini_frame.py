3ecf4bf50230bb27141cff3c5ef02bce6a9a0813
DamonMok <damonmok@Damon.local>
2020-07-12 22:24:01 +0800

    Mini-Web框架_WSGI-mini-web框架_4.将web服务器和逻辑代码解耦

--- mini_frame.py
+++ mini_frame.py
@@ -13,5 +13,14 @@ def other():
     return "other--%s" % time.ctime()
 
 
-def application():
-    return "Hello,Python1!%s" % time.ctime()
+def application(file_name):
+
+    # 把逻辑代码跟web服务器解耦
+    if file_name == "/login.py":
+        return login()
+    elif file_name == "/register.py":
+        return register()
+    elif file_name == "/other.py":
+        return other()
+    else:
+        return "page not found!404!"
